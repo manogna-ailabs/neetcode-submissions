af390c10807904791d4b4f59090f03004d46f2b1
manogna-ailabs <manogna.ailabs@gmail.com>
2026-04-12 21:17:21 +0530

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0 or n==1:
+            return n
+        dp = [0] * (n+1)
+        dp[1], dp[2] = 1, 1
+        if n >= 3:
+            for i in range(3,n+1):
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        return dp[n]
