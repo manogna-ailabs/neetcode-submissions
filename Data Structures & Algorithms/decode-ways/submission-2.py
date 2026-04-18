89e9cd709a87a684b35be6a3b8fa5024f0e953eb
manogna-ailabs <manogna.ailabs@gmail.com>
2026-04-18 23:05:09 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * n
+        def dfs(i):
+            if i == n:
+                return 1
+            if i>n :
+                return 0
+            if s[i] == "0":
+                return 0
+            if dp[i] > 0:
+                return dp[i]
+            dp[i] = dfs(i+1)
+            if int(s[i:i+2]) < 27:
+                dp[i] += dfs(i+2)
+            return dp[i]
+
+        return dfs(0)
+        
