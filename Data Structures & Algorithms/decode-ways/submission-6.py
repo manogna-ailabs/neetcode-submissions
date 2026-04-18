ca4cf11d3b7c2aa370a71a5e2efc0330afc7a29a
manogna-ailabs <manogna.ailabs@gmail.com>
2026-04-18 23:14:46 +0530

    Add: decode-ways - submission-6

---
+++ Data Structures & Algorithms/decode-ways/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp = {}
+        def dfs(i):
+            if i == n:
+                return 1
+            if s[i] == "0":
+                return 0
+            if i in dp:
+                return dp[i]
+            dp[i] = dfs(i+1)
+            if i+2 <= n and (s[i]=="1" or s[i]=="2" and s[i+1] in "0123456"):
+                dp[i] += dfs(i+2)
+            return dp[i]
+
+        return dfs(0)
+        
