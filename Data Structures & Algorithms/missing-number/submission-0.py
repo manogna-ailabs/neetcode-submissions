324a43da080deba10cdd42fe6a073268b47dfe5f
manogna-ailabs <manogna.ailabs@gmail.com>
2026-04-25 23:37:28 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        xorr = n
+        for i in range(n):
+            xorr ^= i
+            xorr ^= nums[i]
+        return xorr
