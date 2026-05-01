d91ffc0df57849ece77078e2409259a73435691e
manogna-ailabs <manogna.ailabs@gmail.com>
2026-05-01 23:15:51 +0530

    Add: online-stock-span - submission-1

---
+++ Data Structures & Algorithms/online-stock-span/submission-1.py
@@ -0,0 +1,20 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stocks = []
+        self.n = 0
+        
+
+    def next(self, price: int) -> int:
+        self.stocks.append(price)
+        self.n += 1
+        
+        n = self.n
+        while n >0 and self.stocks[n-1] <= price:
+            n -= 1
+        return self.n - n
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
