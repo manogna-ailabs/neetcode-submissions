a4be6fdc5c5b9da680f082e127a309901b21bb3e
manogna-ailabs <manogna.ailabs@gmail.com>
2026-07-06 20:17:47 +0530

    Add: split-array-largest-sum - submission-0

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        memo = {}
+
+        def dfs(i, k):
+            if k == 1:
+                return sum(nums[i:])
+            if (i,k) in memo:
+                return memo[(i,k)]
+            best_sum = float('inf')
+            for end in range(i+1, n-k+2):
+                curr_sum = sum(nums[i:end])
+                rem_sum = dfs(end, k-1)
+                max_sum = max(curr_sum, rem_sum)
+                best_sum = min(best_sum, max_sum)
+            memo[(i, k)] = best_sum
+            return best_sum
+
+        return dfs(0, k)
